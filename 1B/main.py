8b2d2cf5728771383666f2c76b530a9b4f8a7113
Kolos1974 <karlovitz.kolos@gmail.com>
2024-02-02 22:38:36 +0100

    1B Hái feladat

---
+++ 1B/main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+print('A 90 m2-es szoba kifestésének nettó munkadíja:', 90*12/60*2*1800)
+
+print('A 90 m2-es szoba kifestésének bruttó munkadíja:', 90*12/60*2*1800*1.27)
